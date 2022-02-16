f28643e6a3197efdf5d8840213b9d1976033af6c
jaimealcalde <84562909+jaimealcalde@users.noreply.github.com>
2022-02-16 12:03:36 +0100

    add firebase variables

--- server/src/middlewares/firebaseConfig.js
+++ server/src/middlewares/firebaseConfig.js
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 //import { createRequire } from "module";
 //const require = createRequire(import.meta.url);
 //const serviceAccount = require("./serviceAccount.json");
-console.log(process.env.FIREBASE_PROJECT_ID)
+//console.log(process.env.FIREBASE_PROJECT_ID)
 
 admin.initializeApp({
   credential: admin.credential.cert({
